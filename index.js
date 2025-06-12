518ecd2a4f28bcbfc32101f795aa46139244c7ac
Mubarrat Choudhury <53148364+mubarratc@users.noreply.github.com>
2025-06-12 11:52:43 +0000

    first commit to this repo

---
+++ index.js
@@ -0,0 +1,31 @@
+// [] Create structure of the page
+// [] Style the page
+// [] Dyanmically grab the content of the menu from the
+// [] Enable the ability for a user to add items to their cart
+// [] Enable the ability for a user to remove items from their cart
+// [] Enable the ability for a user to complete their order
+
+import { menuArray } from "./data.js";
+const menu = document.getElementById("menu");
+console.log(menu);
+
+function getMenuHtml() {
+    let menuHtml = ``;
+    menuArray.forEach(function (item) {
+        menuHtml += `
+        <div class="menu-item">
+            <div class="menu-item-emoji">${item.emoji}</div>
+            <div class="menu-item-description">
+                <h2>${item.name}</h2>
+                <p>${item.ingredients.join(", ")}</p>
+                <h3>${item.price}</h3>
+            </div>
+            <button class="menu-item-add" data-add="${item.id}">+</button>
+        </div>
+        `;
+    });
+    return menuHtml;
+}
+
+console.log(getMenuHtml());
+menu.innerHTML = getMenuHtml(); // This is the same as document.getElementById("menu").innerHTML = getMenuHtml()
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import {defineConfig} from "vite"
+// vite.config.js
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+	server: {
+		host: "0.0.0.0",
+		allowedHosts: [
+			"576c5a80-4dbd-4ae3-a305-c62ee5f6bfa2-00-3st55ogq5z0bx.spock.replit.dev",
+		],
+	},
+});
